2685de0f04f567e566a3bf00a12b0cee903d7aff
kingsley <rawmetal77@gmail.com>
2023-02-04 17:29:29 +0100

    dark mode added

--- Components/script.js
+++ Components/script.js
@@ -3,3 +3,24 @@ const mobileNavs = document.getElementsByClassName("nav-links")[0]
 hamburger.addEventListener("click", () => {
   mobileNavs.classList.toggle("active")
 })
+
+const body = document.querySelector("body");
+const toggleButton = document.querySelector("#toggle-mode");
+
+function toggleDarkMode() {
+  if (body.classList.contains("light-mode")) {
+    body.classList.replace("light-mode", "dark-mode");
+    localStorage.setItem("mode", "dark");
+  } else {
+    body.classList.replace("dark-mode", "light-mode");
+    localStorage.setItem("mode", "light");
+  }
+}
+
+// Check if the user has selected a mode before
+const currentMode = localStorage.getItem("mode");
+if (currentMode === "dark") {
+  body.classList.replace("light-mode", "dark-mode");
+}
+
+toggleButton.addEventListener("click", toggleDarkMode);
